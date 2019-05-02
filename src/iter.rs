3c0c1b4f2c69b69f4f4bba79e6f1e3213fed00dd
Sean McArthur <sean@seanmonstar.com>
2019-05-02 09:35:15 -0700

    add iter::root() function

--- src/iter.rs
+++ src/iter.rs
@@ -9,14 +9,38 @@ use super::{Error, ErrorRef};
 /// ```
 /// let err = errors::wrap("c", errors::wrap("b", "a"));
 ///
-/// let mut iter = errors::iter::chain(&*err);
-/// assert_eq!(iter.next().unwrap().to_string(), "c");
-/// assert_eq!(iter.next().unwrap().to_string(), "b");
-/// assert_eq!(iter.next().unwrap().to_string(), "a");
-/// assert!(iter.next().is_none());
+/// let expected = ["c", "b", "a"];
+///
+/// for (err, &s) in errors::iter::chain(&*err).zip(expected.iter()) {
+///     assert_eq!(err.to_string(), s);
+/// }
 /// ```
 pub fn chain<'a>(err: &'a ErrorRef) -> impl Iterator<Item = &'a ErrorRef> + 'a {
     Iter { err: Some(err) }
+}
+
+/// Get the root source of an `Error`.
+///
+/// If the provided `Error` has a source chain, this will find the last one
+/// in the chain. If there is no chain, returns the same `Error`.
+///
+/// # Example
+///
+/// ```
+/// // Error chain: c -> b -> a (root)
+/// let err = errors::wrap("c", errors::wrap("b", "a"));
+///
+/// assert_eq!(errors::iter::root(&*err).to_string(), "a");
+///
+/// // No chain:
+/// let root = errors::opaque("ninja cat");
+///
+/// assert_eq!(errors::iter::root(&*root).to_string(), "ninja cat");
+/// ```
+pub fn root(err: &ErrorRef) -> &ErrorRef {
+    chain(err)
+        .last()
+        .expect("errors::iter::chain always yields at least 1 item")
 }
 
 /// Get an `Iterator` of the source chain of this error.
@@ -28,10 +52,12 @@ pub fn chain<'a>(err: &'a ErrorRef) -> impl Iterator<Item = &'a ErrorRef> + 'a {
 /// ```
 /// let err = errors::wrap("c", errors::wrap("b", "a"));
 ///
-/// let mut iter = errors::iter::sources(&*err);
-/// assert_eq!(iter.next().unwrap().to_string(), "b");
-/// assert_eq!(iter.next().unwrap().to_string(), "a");
-/// assert!(iter.next().is_none());
+///
+/// let expected = ["b", "a"];
+///
+/// for (err, &s) in errors::iter::sources(&*err).zip(expected.iter()) {
+///     assert_eq!(err.to_string(), s);
+/// }
 /// ```
 pub fn sources(err: &dyn Error) -> impl Iterator<Item = &ErrorRef> {
     Iter { err: err.source() }
